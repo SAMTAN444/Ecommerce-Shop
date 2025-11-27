a27e5a8d32b9ef7b477e8cb624dd9f77a97372cd
notsamueltjy@gmail.com <126168312+SAMTAN444@users.noreply.github.com>
2025-11-27 15:29:34 +0800

    Added Redux for state management

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,12 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { productListReducer, productDetailsReducer } from '../reducers/productReducers';   
+
+
+const store = configureStore({
+    reducer: {
+        productList: productListReducer,
+        productDetails: productDetailsReducer
+    }
+});
+
+export default store;
